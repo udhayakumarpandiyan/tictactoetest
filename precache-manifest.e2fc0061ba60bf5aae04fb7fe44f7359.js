2bb7ad0d9f9ec224646dde2e9528d6d1db8db687
udhayakumar <pudhayakumar@tila.com>
2020-06-29 20:53:38 +0530

    Updates

---
+++ precache-manifest.e2fc0061ba60bf5aae04fb7fe44f7359.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6295ad0c898d031a07430884c281b1e4",
+    "url": "/track/index.html"
+  },
+  {
+    "revision": "25e00c252bb95bb23634",
+    "url": "/track/static/css/2.4d221211.chunk.css"
+  },
+  {
+    "revision": "5b2c6537386fb8bcaaee",
+    "url": "/track/static/css/4.dc862cab.chunk.css"
+  },
+  {
+    "revision": "2538cffcebe102c6b81b",
+    "url": "/track/static/css/main.049d1cf7.chunk.css"
+  },
+  {
+    "revision": "25e00c252bb95bb23634",
+    "url": "/track/static/js/2.f1613c8d.chunk.js"
+  },
+  {
+    "revision": "79c32a37126b751130d1",
+    "url": "/track/static/js/3.03e4f711.chunk.js"
+  },
+  {
+    "revision": "5b2c6537386fb8bcaaee",
+    "url": "/track/static/js/4.77b316d7.chunk.js"
+  },
+  {
+    "revision": "2538cffcebe102c6b81b",
+    "url": "/track/static/js/main.1c98f7a8.chunk.js"
+  },
+  {
+    "revision": "d129f8ca4b061f00062e",
+    "url": "/track/static/js/runtime~main.c11a1c2a.js"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/track/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "3a811e97afb3a0186c5653783ead87ac",
+    "url": "/track/static/media/bg.3a811e97.png"
+  },
+  {
+    "revision": "10ebfb6e90a620d977bffc10b079502b",
+    "url": "/track/static/media/dummy.10ebfb6e.jpg"
+  },
+  {
+    "revision": "2be60fc34e3ac302bbeae8d646daa63b",
+    "url": "/track/static/media/my-photo.2be60fc3.jpg"
+  },
+  {
+    "revision": "db77fecb80c1b5d5ed6ac1785220df3f",
+    "url": "/track/static/media/pmk.db77fecb.png"
+  },
+  {
+    "revision": "456c3da17833a478c50de18ff97ca6a9",
+    "url": "/track/static/media/robotocondensed-regular-webfont.456c3da1.woff2"
+  },
+  {
+    "revision": "8c231fd9317cb00e06f899d132a91a63",
+    "url": "/track/static/media/robotocondensed-regular-webfont.8c231fd9.woff"
+  },
+  {
+    "revision": "fc4b82e57f17777e5101d152d0d6d631",
+    "url": "/track/static/media/slide.fc4b82e5.png"
+  }
+]);
